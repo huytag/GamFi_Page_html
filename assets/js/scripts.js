27692777f2f6cd242b9f8bc7c507e4c5d5c12e5e
huytag <huytagicloud@gmail.com>
2022-05-20 15:02:36 +0700

    fix couting Number and font-weight

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -10,38 +10,40 @@
       countDownTime();
       countingNumber();
       AOS.init({
-        duration: 500
+        duration: 500,
       });
       $(window).scroll(function () {
         countingNumber();
       });
     });
 
-
+    let setOne = true;
     function countingNumber() {
       let valueDisplays = document.querySelectorAll(".counter");
-      
-      if($(window).scrollTop() > 3500) {
-        valueDisplays.forEach((valueDisplay) => {
-        let startNum = 0;
-        let endNum = parseInt(valueDisplay.getAttribute("data-value"));
-        let duration = Math.floor(1500/endNum);
-        let counter = setInterval(() => {
-          startNum += 1;
-          valueDisplay.textContent =  startNum;
-          if (startNum == endNum) {
-            clearInterval(counter);
-          }
-        },duration)
-      })
-      } 
+      if ($(window).scrollTop() > 3400) {
+        if (setOne) {
+          setOne = false;
+          valueDisplays.forEach((valueDisplay) => {
+            let startNum = 0;
+            let endNum = parseInt(valueDisplay.getAttribute("data-value"));
+            let duration = Math.floor(1500 / endNum);
+            let counter = setInterval(() => {
+              startNum += 1;
+              valueDisplay.textContent = startNum;
+              if (startNum == endNum) {
+                clearInterval(counter);
+              }
+            }, duration);
+          });
+        }
+      }
     }
 
     function countDownTime() {
       let hours = 22;
       let minutes = 50;
       let seconds = 12;
-      
+
       const time = setInterval(() => {
         seconds = seconds - 1;
         if (seconds == 0) {
@@ -59,9 +61,8 @@
         $(".timer .hours").html(`${hours}<span>H</span>`);
         $(".timer .minutes").html(`${minutes}<span>M</span>`);
         $(".timer .seconds").html(`${seconds}<span>S</span>`);
-      },1000)
+      }, 1000);
     }
-
 
     function teamItemHover() {
       $(".team-item").hover(
@@ -71,7 +72,7 @@
         function () {
           $(".team-item").removeClass("active-shape");
         }
-      )
+      );
       $(".previous-item").hover(
         function () {
           $(".previous-item").addClass("active-shape");
@@ -79,7 +80,7 @@
         function () {
           $(".previous-item").removeClass("active-shape");
         }
-      )
+      );
       $(".project-item").hover(
         function () {
           $(".project-item").addClass("active-shape");
@@ -87,13 +88,12 @@
         function () {
           $(".project-item").removeClass("active-shape");
         }
-      )
+      );
     }
-
 
     function onScrollTop() {
       const elClick = $("#srcoll-Up");
-      if(elClick.length > 0) {
+      if (elClick.length > 0) {
         elClick.on("click", () => {
           $(window).scrollTop(0);
         });
@@ -105,37 +105,34 @@
       const elBtn = $(".nav-expander");
       if (elBtn.length > 0) {
         elBtn.on("click", () => {
-            elHamber.addClass("is-active");
+          elHamber.addClass("is-active");
         });
       }
     }
-
 
     function onCloseHamberMobile() {
       const elHamber = $(".hamber-mb");
       const elBtn = $(".btn-close");
-      if(elBtn.length > 0 ) {
+      if (elBtn.length > 0) {
         elBtn.on("click", () => {
           elHamber.removeClass("is-active");
-        })
+        });
       }
     }
 
     function bannerSlickInner() {
-      $(".banner-slider-inner")
-        .not(".slick-initialized")
-        .slick({
-          dots: true,
-          infinite: true,
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          prevArrow: false,
-          nextArrow: false,
-          // autoplay: true,
-          arrows: true,
-          autoplaySpeed: 0,
-					speed: 2500,
-        });
+      $(".banner-slider-inner").not(".slick-initialized").slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: false,
+        nextArrow: false,
+        // autoplay: true,
+        arrows: true,
+        autoplaySpeed: 0,
+        speed: 2500,
+      });
     }
 
     function liveSlickPartNer() {
@@ -152,7 +149,7 @@
           autoplay: true,
           arrows: true,
           autoplaySpeed: 0,
-					speed: 5000,
+          speed: 5000,
           responsive: [
             {
               breakpoint: 1200,
